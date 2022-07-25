a4daf3f97ade02707edbf69557f3f8e3ee5388d1
Kseniia Konoshko <ksushka.k.393@gmail.com>
2022-07-25 21:14:43 +0300

    added area + city, rewiews - digits

--- create_dict_rows.py
+++ create_dict_rows.py
@@ -21,7 +21,12 @@ def create_dict_rows():
 
         for hotel in hotels:
             name = hotel.findChild('div', class_='fcab3ed991 a23c043802').text
+
             location = hotel.findChild('span', class_='f4bd0794db b4273d69aa').text
+            if re.search(r'(\,\s)', location):
+                area, city = location.split(', ')
+            else:
+                area, city = None, location
 
             price = hotel.findChild('span', class_='fcab3ed991 bd73d13072').text
             price = int(re.findall(r'\d+', price)[0])
@@ -35,13 +40,13 @@ def create_dict_rows():
 
             reviews = hotel.findChild('div', class_='d8eab2cf7f c90c0a70d3 db63693c62')
             if reviews is not None:
-                reviews = reviews.text
+                reviews = int(re.findall(r'\d+', reviews.text.replace(',', ''))[0])
             else:
                 reviews = None
 
             url = hotel.findChild('a').get('href')
 
-            dict_rows.append({'hotel_name': name, 'location': location,
+            dict_rows.append({'hotel_name': name, 'area': area, 'city': city,
                               'price': price, 'score': score, 'reviews': reviews, 'url': url})
 
 
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import csv
 from create_dict_rows import create_dict_rows
-
+import re
 
 def write_to_csv():
     """
     Function writes to csv file all gathered data as a table
     """
     dict_rows = create_dict_rows()
-    hotel_info = ['hotel_name', 'location', 'price', 'score', 'reviews', 'url']
+    hotel_info = ['hotel_name', 'area', 'city', 'price', 'score', 'reviews', 'url']
 
     with open('hotels.csv', 'w', encoding='utf-8') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=hotel_info)
