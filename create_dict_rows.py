9ab6c93085c813121bea44dea5cf093cddef7c02
Anna Lelchuk <lelc-anya@yandex.ru>
2022-07-27 21:51:38 +0300

    parsing features inside hotel url

--- create_dict_rows.py
+++ create_dict_rows.py
@@ -1,4 +1,5 @@
 import grequests
+import requests
 import conf as CFG
 from get_urls import get_urls
 from bs4 import BeautifulSoup
@@ -21,6 +22,7 @@ def create_dict_rows():
 
         for hotel in hotels:
             name = hotel.findChild('div', class_='fcab3ed991 a23c043802').text
+            print(name)
 
             location = hotel.findChild('span', class_='f4bd0794db b4273d69aa').text
             if re.search(r'(\,\s)', location):
@@ -43,12 +45,101 @@ def create_dict_rows():
                 reviews = int(re.findall(r'\d+', reviews.text.replace(',', ''))[0])
             else:
                 reviews = None
+            print(f'reviews: {reviews}')
 
             url = hotel.findChild('a').get('href')
 
-            dict_rows.append({'hotel_name': name, 'area': area, 'city': city,
-                              'price': price, 'score': score, 'reviews': reviews, 'url': url})
+#--------------------------------------------------------------
+            rs_hotel_scores = requests.get(url, headers=CFG.HEADERS)
 
+            soup = BeautifulSoup(rs_hotel_scores.text, "html.parser")
+            facilities_scores = soup.find_all('span', class_='c-score-bar__title')
+            score_dict={}
+            for fac_score in facilities_scores:
+                score_dict[fac_score.text.rstrip("\xa0")] = fac_score.find_next_sibling("span").text
+
+            if 'Location' in score_dict:
+                hotel_loc_score = float(score_dict['Location'])
+            else:
+                hotel_loc_score = None
+            print(f'location socre: {hotel_loc_score}')
+
+            if 'Staff' in score_dict:
+                staff_score = float(score_dict['Staff'])
+            else:
+                staff_score = None
+            print(f'staff score: {staff_score}')
+
+            if 'Free WiFi' in score_dict:
+                wifi_score = float(score_dict['Free WiFi'])
+            else:
+                wifi_score = None
+            print(f'wifi_score: {wifi_score}')
+
+            if 'Cleanliness' in score_dict:
+                clean_score = float(score_dict['Cleanliness'])
+            else:
+                clean_score = None
+            print(f'clean_score: {clean_score}')
+
+# --------------------------------------------------------------
+            rs_hotel_facilities = requests.get(url, headers=CFG.HEADERS)
+
+            soup = BeautifulSoup(rs_hotel_facilities.text, "html.parser")
+            facilities = soup.find('div', class_='hotel-facilities__list')
+            facilities_list = [facility.text.split('\n') for facility in facilities]
+            facilities_list = sum(facilities_list, [])
+            if 'Non-smoking rooms' in facilities_list:
+                non_smoking = True
+            else:
+                non_smoking = False
+            print(f'non smoke: {non_smoking}')
+
+            if 'Business centre' in facilities_list:
+                business_center = True
+            else:
+                business_center = False
+            print(f'business cent: {business_center}')
+
+            if 'Free parking' in facilities_list:
+                free_parking = True
+            else:
+                free_parking = False
+            print(f'parking: {free_parking}')
+
+            if '24-hour front desk' in facilities_list:
+                front_desk_24_7 = True
+            else:
+                front_desk_24_7 = False
+            print(f'24 7: {front_desk_24_7}')
+
+            if 'Laundry' in facilities_list:
+                laundry = True
+            else:
+                laundry = False
+            print(f'Laundry: {laundry}')
+
+            if 'Airport shuttle' in facilities_list:
+                shuttle = True
+            else:
+                shuttle = False
+            print(f'shuttle: {shuttle}')
+
+
+
+
+# --------------------------------------------------------------
+
+            dict_rows.append({'hotel_name': name, 'area': area, 'city': city,
+                              'price': price, 'score': score, 'reviews': reviews,
+                              'hotel_loc_score': hotel_loc_score, 'staff_score': staff_score,
+                              'wifi_score': wifi_score, 'clean_score': clean_score,
+                              'non_smoking': non_smoking, 'business_center': business_center,
+                              'free_parking': free_parking, 'front_desk_24_7': front_desk_24_7,
+                              'laundry': laundry, 'shuttle': shuttle
+                               })
+            # print(f'dict: {dict_rows}')
 
 
     return dict_rows
+
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import csv
 from create_dict_rows import create_dict_rows
 import re
+import time
+#fake-useragent
 
 def write_to_csv():
     """
     Function writes to csv file all gathered data as a table
     """
     dict_rows = create_dict_rows()
-    hotel_info = ['hotel_name', 'area', 'city', 'price', 'score', 'reviews', 'url']
+    hotel_info = ['hotel_name', 'area', 'city', 'price', 'score', 'reviews', 'hotel_loc_score', 'staff_score',
+                  'wifi_score', 'clean_score', 'non_smoking', 'business_center', 'free_parking', 'front_desk_24_7',
+                  'laundry', 'shuttle']
+
+    # dict_rows.append({'hotel_name': name, 'area': area, 'city': city,
+    #                   'price': price, 'score': score, 'reviews': reviews,
+    #                   'hotel_loc_score': hotel_loc_score, 'staff_score': staff_score,
+    #                   'wifi_score': wifi_score, 'clean_score': clean_score,
+    #                   'non_smoking': non_smoking, 'business_center': business_center,
+    #                   'free_parking': free_parking, 'front_desk_24_7': front_desk_24_7,
+    #                   'laundry': laundry, 'shuttle': shuttle
+    #                   })
 
     with open('hotels.csv', 'w', encoding='utf-8') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=hotel_info)
@@ -18,6 +31,7 @@ def write_to_csv():
 def main():
     write_to_csv()
 
-
+start_time = time.time()
 if __name__ == '__main__':
     main()
+print("--- %s seconds ---" % (time.time() - start_time))
