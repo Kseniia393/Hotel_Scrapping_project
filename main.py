d17d68b06097e76131d48889e4907a73a882b989
Alexey Konev <alexey.konev@gmail.com>
2022-08-17 16:27:30 +0300

    add logger

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 # from write_to_db import write_to_db
 import time
 import argparse
+import requests
+import logging
+import sys
 import conf as CFG
 from create_DB import create_db, create_db_tables
 from get_urls import get_urls
 from write_to_db import write_to_db
-import requests
 from bs4 import BeautifulSoup
 from tqdm.auto import tqdm
 from scrap_data import scrap_facilities, scrape_hotel, scrap_scores
 from api import get_hotel_google_score
+
+
+# book_scrap_log setting
+logger = logging.getLogger('book_scrap_log')
+logger.setLevel(logging.DEBUG)
+# standard log output
+stream_formatter = logging.Formatter('%(asctime)s - %(message)s', datefmt='%I:%M:%S')
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(stream_formatter)
+logger.addHandler(stream_handler)
+# file log out
+file_formatter = logging.Formatter(
+    '%(asctime)s-%(levelname)s-FUNC:%(funcName)s-LINE:%(lineno)d-%(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S')
+file_handler = logging.FileHandler(CFG.LOG_FILE + '.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def parse_cli():
@@ -35,16 +56,17 @@ def parse_cli():
 
 
 def main():
-    '''
+    """
     Acts as an orchestrator to program functions.
     Calls different functions to get CLI apguments, generate urls, scrap data, write to the DB.
     :return: None
-    '''
+    """
 
     hotel_dict = {}
     hotel_dict['city'], hotel_dict['check_in_date'], hotel_dict['check_out_date'], hotel_dict[
         'adults'], CFG.PASSWORD = parse_cli()
 
+    logger.info('Creating database with tables')
     # check if DB and tables are in place, if not, create them:
     create_db(CFG.PASSWORD)
     create_db_tables(CFG.PASSWORD)
@@ -52,9 +74,11 @@ def main():
     # generate all urls of the search result:
     url_list = get_urls(hotel_dict)
 
+    logger.info('Creating list of responses for all hotels')
     # create list of responses for all search urls:
     response = [requests.get(url, headers=CFG.HEADERS) for url in url_list]
 
+    logger.info('Parsing hotels...')
     # get hotel data from all search urls:
     for i in tqdm(range(len(response))):
         soup = BeautifulSoup(response[i].text, "html.parser")
@@ -64,7 +88,8 @@ def main():
             hotel_dict['hotel_title'], hotel_dict['hotel_area'], hotel_dict['hotel_city'], hotel_dict['price'], \
             hotel_dict['hotel_score'], hotel_dict['hotel_review'], hotel_dict['url'] = scrape_hotel(hotel)
 
-            print(hotel_dict['hotel_title'])
+            logger.debug(f"Parsing hotel: {hotel_dict['hotel_title']}")
+            # print(hotel_dict['hotel_title'])
 
             rs_hotel = requests.get(hotel_dict['url'], headers=CFG.HEADERS)
             hotel_dict['soup'] = BeautifulSoup(rs_hotel.text, "html.parser")
@@ -82,10 +107,12 @@ def main():
             try:
                 hotel_dict['hotel_google_score'] = get_hotel_google_score(query_for_api)
             except KeyError:
+                logger.debug("doesn't recieved hotel_google_score")
                 hotel_dict['hotel_google_score'] = None
 
             # write all collected data to the DB:
             write_to_db(CFG.PASSWORD, hotel_dict)
+    logger.info('TADAAAAAM')
 
 
 start_time = time.time()
